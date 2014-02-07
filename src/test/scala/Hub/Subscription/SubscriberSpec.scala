978c098104a46789449da53d2fb1f7cda4704dc1
rmacdonaldsmith <rmacdonaldsmith@yahoo.co.uk>
2014-02-07 15:29:17 -0600

    factored unit tests to look a little nicer

--- src/test/scala/Hub/Subscription/SubscriberSpec.scala
+++ src/test/scala/Hub/Subscription/SubscriberSpec.scala
@@ -4,6 +4,7 @@ import akka.actor.{ActorRef, Props}
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import org.specs2.mutable._
+import Hub.Subscription.SubscriberSpec.FakeWebClient
 
 object SubscriberSpec {
 
@@ -12,32 +13,57 @@ object SubscriberSpec {
       override def client = webClient
     })
 
-  object FakeWebClient extends WebClient {
-    override def postUpdate(url: String, payload: Any, topic: String): Future[Int] = Future.successful(201)
-    override def postUnSubscribe(url: String, topic: String): Future[Int] = Future.failed(PostUpdateFailed(500))
+  class FakeWebClient(updateCallback: (String, Any, String) => Future[Int], unSubscribeCallback: (String, String) => Future[Int]) extends WebClient {
+    override def postUpdate(url: String, payload: Any, topic: String): Future[Int] = updateCallback(url, payload, topic)
+    override def postUnSubscribe(url: String, topic: String): Future[Int] = unSubscribeCallback(url, topic)
   }
+}
+
+trait WebClientScope extends Specs2Context with After {
+  var called = false
+
+  def fakeWebClient = {
+    new FakeWebClient((url, payload, topic) => {
+      called = true
+      Future.successful(204)
+    },
+      (url, topic) => {
+        called = true
+        Future.successful(204)
+      })
+  }
+
+  override def after = called = true
 }
 
 class SubscriberSpec extends Specification {
 
   import SubscriberSpec._
-  
+
   "A subscriber" should {
 
-    "forward the new event to the callback url" in new Specs2Context {
-      val fakeClient = FakeWebClient
+    "forward the new event to the callback url" in new WebClientScope {
       val callbackUrl = "http://localhost:9000/UserEvents"
-      val subscriber: ActorRef = system.actorOf(buildTestSubscriber(callbackUrl, "unSubscribeUrl", "aTopic", fakeClient))
+      val subscriber: ActorRef = system.actorOf(buildTestSubscriber(callbackUrl, "unSubscribeUrl", "aTopic", fakeWebClient))
 
       subscriber ! Subscriber.Update(Nil)
 
       within(200 milli) {
         expectNoMsg
+        called must beTrue
       }
     }
 
-    "call the unsubscriber url" in new Specs2Context {
-      
+    "call the unsubscriber url" in new WebClientScope {
+      val unsubscribeUrl = "http://localhost:9000/UnSubscribe"
+      val subscriber: ActorRef = system.actorOf(buildTestSubscriber("callbackUrl", unsubscribeUrl, "aTopic", fakeWebClient))
+
+      subscriber ! Subscriber.UnSubscribe
+
+      within(200 milli) {
+        expectNoMsg
+        called must beTrue
+      }
     }
   }
 }
